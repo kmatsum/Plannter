be866ffaca611df8284c9df062d644e7a7628d02
clark462 <1997clark@gmail.com>
2019-10-20 16:54:42 -0400

    Added some new activities and some functionality

--- app/src/main/java/com/c355_project/plannter/MainActivity.java
+++ app/src/main/java/com/c355_project/plannter/MainActivity.java
@@ -2,13 +2,27 @@ package com.c355_project.plannter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnStart;
+    Intent openStartScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openStartScreen = new Intent(getApplicationContext(), Start_Screen.class);
+                startActivity(openStartScreen);
+            }
+        });
     }
 }
