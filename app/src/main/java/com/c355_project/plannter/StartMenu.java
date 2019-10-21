dda574c2e3133229f64a8971314486a695d51292
kazushi1712 <44615506+kazushi1712@users.noreply.github.com>
2019-10-21 18:42:12 -0400

    - Refactored some names in the project

--- app/src/main/java/com/c355_project/plannter/StartMenu.java
+++ app/src/main/java/com/c355_project/plannter/StartMenu.java
@@ -6,11 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class StartMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start_menu);
     }
 
 
