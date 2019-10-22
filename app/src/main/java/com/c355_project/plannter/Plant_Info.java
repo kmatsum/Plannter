676ccd46283e622459d6d60880c4d6c34f354aed
kazushi1712 <44615506+kazushi1712@users.noreply.github.com>
2019-10-22 09:53:26 -0400

    - Initial creation of the Plant Info Page

---
+++ app/src/main/java/com/c355_project/plannter/Plant_Info.java
@@ -0,0 +1,13 @@
+package com.c355_project.plannter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Plant_Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plant_info);
+    }
+}
