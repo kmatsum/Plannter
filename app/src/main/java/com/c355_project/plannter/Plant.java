6eaac9367487f9605a72da5c2853f41e9a4f7d57
Rachel <rrettig@purdue.edu>
2019-10-23 21:12:57 -0400

    ADDED PLANTS TO DB ============================== Added Fall attributes to Plant class. Updated default plants to include new attributes.

--- app/src/main/java/com/c355_project/plannter/Plant.java
+++ app/src/main/java/com/c355_project/plannter/Plant.java
@@ -8,15 +8,25 @@ public class Plant{
 
     /* VARIABLE DESCRIPTIONS =======================================================================
 
-    id:             Auto-generated primary key (starts at 1, increments by 1)
-    plantName:      Name of plant
-    seedCompany:    Seed company, preloaded plants will have value "General"
-    firstPlantDate: # of weeks BEFORE Avg Last Frost Date that the plant should be planted
-    harvestMinDate: Minimum # of weeks AFTER Avg Last Frost Date that the plant can be harvested
-    harvestRange:   # of weeks the plant may be ready to harvest after harvestMinDate
-    seedIndoorDate: # of weeks BEFORE Avg Last Frost Date that the plant should be seeded indoors
-                    This is not always required. Thus, assign a value of 52 if not required.
-    notes:          Optional place to add notes regarding plant
+    id:                 Auto-generated primary key (starts at 1, increments by 1).
+    plantName:          Name of plant.
+    seedCompany:        Seed company, preloaded plants will have value "General".
+    firstPlantDate:     # of weeks BEFORE Avg Last Spring Frost that the plant should be planted.
+                        A value of 52 means the crop is not recommended for Spring.
+    weeksToHarvest:     # of weeks between planted date and first expected harvest date.
+    spHarvestRange:     # of weeks the plant may be ready to harvest after weeksToHarvest.
+                        A value of 52 means the crop is not recommended for Spring.
+    spSeedIndoorDate:   # of weeks BEFORE Avg Last Spring Frost that the plant should be seeded indoors
+                        A value of 52 means indoor seeding is not required,
+                        or the crop is not recommended for Spring.
+    lastPlantDate:      # of weeks BEFORE Avg First Fall Frost that the plant should be planted
+                        A value of 52 means the crop is not recommended for Fall.
+    faHarvestRange:     # of weeks the plant may be ready to harvest after faWeeksToHarvest
+                        A value of 52 means the crop is not recommended for Fall.
+    faSeedIndoorDate:   # of weeks BEFORE Avg First Fall Frost that the plant should be seeded indoors
+                        A value of 52 means indoor seeding is not required,
+                        or the crop is not recommended for Fall.
+    notes:              Optional place to add notes regarding plant
 
     ============================================================================================= */
 
@@ -25,19 +35,29 @@ public class Plant{
     private String plantName;
     private String seedCompany;
     private int firstPlantDate;
-    private int harvestMinDate;
-    private int harvestRange;
-    private int seedIndoorDate;
+    private int weeksToHarvest;
+    private int spHarvestRange;
+    private int spSeedIndoorDate;
+    private int lastPlantDate;
+    private int faHarvestRange;
+    private int faSeedIndoorDate;
     private String notes;
 
     //CONSTRUCTOR ==================================================================================
-    public Plant(String plantName, String seedCompany, int firstPlantDate, int harvestMinDate, int harvestRange, int seedIndoorDate, String notes){
+
+
+    public Plant(String plantName, String seedCompany, int firstPlantDate,
+                 int weeksToHarvest, int spHarvestRange, int spSeedIndoorDate,
+                 int lastPlantDate, int faHarvestRange, int faSeedIndoorDate, String notes) {
         this.plantName = plantName;
         this.seedCompany = seedCompany;
         this.firstPlantDate = firstPlantDate;
-        this.harvestMinDate = harvestMinDate;
-        this.harvestRange = harvestRange;
-        this.seedIndoorDate = seedIndoorDate;
+        this.weeksToHarvest = weeksToHarvest;
+        this.spHarvestRange = spHarvestRange;
+        this.spSeedIndoorDate = spSeedIndoorDate;
+        this.lastPlantDate = lastPlantDate;
+        this.faHarvestRange = faHarvestRange;
+        this.faSeedIndoorDate = faSeedIndoorDate;
         this.notes = notes;
     }
 
@@ -74,28 +94,52 @@ public class Plant{
         return firstPlantDate;
     }
 
-    public void setHarvestMinDate(int harvestMinDate) {
-        this.harvestMinDate = harvestMinDate;
+    public void setWeeksToHarvest(int weeksToHarvest) {
+        this.weeksToHarvest = weeksToHarvest;
     }
 
-    public int getHarvestMinDate() {
-        return harvestMinDate;
+    public int getWeeksToHarvest() {
+        return weeksToHarvest;
     }
 
-    public void setHarvestRange(int harvestRange) {
-        this.harvestRange = harvestRange;
+    public void setSpHarvestRange(int spHarvestRange) {
+        this.spHarvestRange = spHarvestRange;
     }
 
-    public int getHarvestRange() {
-        return harvestRange;
+    public int getSpHarvestRange() {
+        return spHarvestRange;
     }
 
-    public void setSeedIndoorDate(int seedIndoorDate) {
-        this.seedIndoorDate = seedIndoorDate;
+    public void setSpSeedIndoorDate(int spSeedIndoorDate) {
+        this.spSeedIndoorDate = spSeedIndoorDate;
     }
 
-    public int getSeedIndoorDate() {
-        return seedIndoorDate;
+    public int getSpSeedIndoorDate() {
+        return spSeedIndoorDate;
+    }
+
+    public void setLastPlantDate(int lastPlantDate) {
+        this.lastPlantDate = lastPlantDate;
+    }
+
+    public int getLastPlantDate() {
+        return lastPlantDate;
+    }
+
+    public void setFaHarvestRange(int faHarvestRange) {
+        this.faHarvestRange = faHarvestRange;
+    }
+
+    public int getFaHarvestRange() {
+        return faHarvestRange;
+    }
+
+    public void setFaSeedIndoorDate(int faSeedIndoorDate) {
+        this.faSeedIndoorDate = faSeedIndoorDate;
+    }
+
+    public int getFaSeedIndoorDate() {
+        return faSeedIndoorDate;
     }
 
     public void setNotes(String notes) {
