258e0059065830baed6020b26cef8ddbb374ed94
Rachel <rrettig@purdue.edu>
2020-04-12 22:12:04 -0400

    Updated Note.java to include: - Variable descriptions - Composite primary key of noteID and logID - Indices of foreign key logID - Constructor - Gets & Sets
    Note: Ignored attributes are NOT included in default constructors, per
          Room's requirements.
    
    Current Dilemmas:
    - For the composite key to be declared, it must be at the top after
      @Entity and passed as a string of column names. I'm not sure if there
      is a way to autoGenerate the noteID if it is part of a composite key.
    - I'm not sure if we are properly representing a 1 to many relationship.
      Per my research, there is no native solution in Room. I plan to later
      test the current implementation and change it based on this link
      if necessary: https://stackoverflow.com/questions/44667160/android-room-insert-relation-entities-using-room

--- app/src/main/java/com/c355_project/plannter/Note.java
+++ app/src/main/java/com/c355_project/plannter/Note.java
@@ -2,23 +2,88 @@ package com.c355_project.plannter;
 
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
 import static androidx.room.ForeignKey.CASCADE;
 
-@Entity(foreignKeys = @ForeignKey(entity = Log.class,
+@Entity(primaryKeys = {"noteID", "logID"},
+        foreignKeys = @ForeignKey(entity = Log.class,
         parentColumns = "logID",
         childColumns = "logID",
-        onDelete = CASCADE))
+        onDelete = CASCADE),
+        indices = {@Index(value="logID")})
 public class Note {
 
     /* VARIABLE DESCRIPTIONS =======================================================================
 
     noteID:             Auto-generated primary key (starts at 1, increments by 1).
+    logID:              Foreign key to corresponding Log object.
+    noteType:           String representing the type of note object. Values are limited to:
+                        1) Simple - no image or audio recording, just noteText (noteFilepath = null).
+                        2) Image - an image, with or without noteText.
+                        3) Audio - an audio recording, with or without noteText.
+    noteText:           A string of text notes entered by the user (optional if type = Image or Audio).
+    noteFilepath:       The filepath to the corresponding media file (null when type = Simple).
 
     ============================================================================================= */
 
-    @PrimaryKey(autoGenerate = true)
+    //@PrimaryKey(autoGenerate = true)
     private int noteID;
     private int logID;
+    private String noteType;
+    private String noteText;
+    private String noteFilepath;
+
+//CONSTRUCTOR ======================================================================================
+
+    public Note(int noteID, int logID, String noteType, String noteText, String noteFilepath) {
+        this.noteID = noteID;
+        this.logID = logID;
+        this.noteType = noteType;
+        this.noteText = noteText;
+        this.noteFilepath = noteFilepath;
+    }
+
+//GETS & SETS ======================================================================================
+
+    public int getNoteID() {
+        return noteID;
+    }
+
+    public void setNoteID(int noteID) {
+        this.noteID = noteID;
+    }
+
+    public int getLogID() {
+        return logID;
+    }
+
+    public void setLogID(int logID) {
+        this.logID = logID;
+    }
+
+    public String getNoteType() {
+        return noteType;
+    }
+
+    public void setNoteType(String noteType) {
+        this.noteType = noteType;
+    }
+
+    public String getNoteText() {
+        return noteText;
+    }
+
+    public void setNoteText(String noteText) {
+        this.noteText = noteText;
+    }
+
+    public String getNoteFilepath() {
+        return noteFilepath;
+    }
+
+    public void setNoteFilepath(String noteFilepath) {
+        this.noteFilepath = noteFilepath;
+    }
 }
