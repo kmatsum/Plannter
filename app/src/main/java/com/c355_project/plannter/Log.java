b02367d736daeb8183b209252b0d0da145d7f11e
Rachel <rrettig@purdue.edu>
2020-04-12 22:12:48 -0400

    Updated Log.java to include: - Variable descriptions - Indices of foreign key plantID - Constructor - Gets & Sets
    Note: Ignored attributes are NOT included in default constructors, per
          Room's requirements.
    
    Current Dilemmas:
    - I'm not sure if we are properly representing a 1 to many relationship.
      Per my research, there is no native solution in Room. I plan to later
      test the current implementation and change it based on this link
      if necessary: https://stackoverflow.com/questions/44667160/android-room-insert-relation-entities-using-room

--- app/src/main/java/com/c355_project/plannter/Log.java
+++ app/src/main/java/com/c355_project/plannter/Log.java
@@ -3,6 +3,7 @@ package com.c355_project.plannter;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
 import androidx.room.Ignore;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
 import java.util.ArrayList;
@@ -12,12 +13,17 @@ import static androidx.room.ForeignKey.CASCADE;
 @Entity(foreignKeys = @ForeignKey(entity = Plant.class,
         parentColumns = "plantID",
         childColumns = "plantID",
-        onDelete = CASCADE))
+        onDelete = CASCADE),
+        indices = {@Index(value="plantID")})
 public class Log {
 
     /* VARIABLE DESCRIPTIONS =======================================================================
 
     logID:              Auto-generated primary key (starts at 1, increments by 1).
+    plantID:            Foreign key to corresponding Plant object.
+    notes:              ArrayList of corresponding Note objects, dynamically read from the database
+                        at runtime. This value is ignored by Room and NOT kept track of in the
+                        database. A custom query is run to get the results based on matching logIDs.
 
     ============================================================================================= */
 
@@ -25,5 +31,37 @@ public class Log {
     private int logID;
     private int plantID;
     @Ignore
-    ArrayList<Note> notes;
+    private ArrayList<Note> notes;
+
+//CONSTRUCTOR ======================================================================================
+    public Log(int logID, int plantID) {
+        this.logID = logID;
+        this.plantID = plantID;
+    }
+
+//GETS & SETS ======================================================================================
+
+    public int getLogID() {
+        return logID;
+    }
+
+    public void setLogID(int logID) {
+        this.logID = logID;
+    }
+
+    public int getPlantID() {
+        return plantID;
+    }
+
+    public void setPlantID(int plantID) {
+        this.plantID = plantID;
+    }
+
+    public ArrayList<Note> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(ArrayList<Note> notes) {
+        this.notes = notes;
+    }
 }
