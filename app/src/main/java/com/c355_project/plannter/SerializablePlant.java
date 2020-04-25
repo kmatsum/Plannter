3dc68b5cb87e28365ed81d84a6a45bebc02e80fd
kazushi1712 <44615506+kazushi1712@users.noreply.github.com>
2020-04-25 18:47:34 -0400

    == NOT STABLE == - Changes architechture to make the SerializablePlant actually serializable

---
+++ app/src/main/java/com/c355_project/plannter/SerializablePlant.java
@@ -0,0 +1,50 @@
+package com.c355_project.plannter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class SerializablePlant implements Serializable {
+    char[] plantName;
+    char[] seedCompany;
+    int firstPlantDate;
+    int weeksToHarvest;
+    int harvestRange;
+    int seedIndoorDate;
+    int lastPlantDate;
+    char[] notes;
+    char[] photoPath;
+    boolean raisedRows;
+    boolean raisedHills;
+    int distBetweenPlants;
+    double seedDepth;
+
+    public SerializablePlant(char[] plantName, char[] seedCompany, int firstPlantDate,
+                             int weeksToHarvest, int harvestRange, int seedIndoorDate, int lastPlantDate,
+                             char[] notes, char[] photoPath, boolean raisedRows, boolean raisedHills,
+                             int distBetweenPlants, double seedDepth)
+    {
+        this.plantName = plantName;
+        this.seedCompany = seedCompany;
+        this.firstPlantDate = firstPlantDate;
+        this.weeksToHarvest = weeksToHarvest;
+        this.harvestRange = harvestRange;
+        this.seedIndoorDate = seedIndoorDate;
+        this.lastPlantDate = lastPlantDate;
+        this.notes = notes;
+        this.photoPath = photoPath;
+        this.raisedRows = raisedRows;
+        this.raisedHills = raisedHills;
+        this.distBetweenPlants = distBetweenPlants;
+        this.seedDepth = seedDepth;
+    }
+
+    public byte[] serialize() throws IOException {
+        ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArray);
+        objectOutputStream.writeObject(SerializablePlant.this);
+        System.out.println("[DEBUG]: serializablePlant was Serialized...");
+        return byteArray.toByteArray();
+    }
+}
